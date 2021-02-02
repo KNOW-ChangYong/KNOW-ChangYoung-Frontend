d9f3edc58fab67a8d9b0e4ddab1ca3a4c639f678
eungyeole <eungyeole@naver.com>
2021-02-02 14:53:47 +0900

    UPDATE ; 출석알림 분기처리

--- src/components/Kakao/Kakao.tsx
+++ src/components/Kakao/Kakao.tsx
@@ -1,32 +1,42 @@
+import axios from "axios";
+
 declare global {
     interface Window {
       Kakao: any;
     }
   }
+function paginate(array : any, page_size : number, page_number : number) {
+    return array.slice((page_number - 1) * page_size, page_number * page_size);
+}
 function kakao(){
-    function sendDefault(){
+    async function sendDefault(){
+        const { data } = await axios.get("https://changapi.eungyeol.live/attendance/attendancestatus")
+        console.log(`${(data.lastDayGraph*100).toFixed(1)}% -> ${(data.todayGraph*100).toFixed(1)}% ( ${(data.todayGraph*100-data.lastDayGraph*100).toFixed(1)}% )`)
         const now:Date = new Date();
         if(now.getHours()===8){
-            const content = [
-                {
-                    title: '취미의 특징, 탁구',
-                    description: '스포츠',
+            let content:any=[];
+            for(let i=1;i<=data.studentResponses.length;i++){
+                if(paginate(data.studentResponses,3,i).length===0) break;
+                content=[...content,{
+                    title: i===1 ? '최종 미출석자 : ' : 'and',
+                    description: `${(paginate(data.studentResponses,3,i).map((i : any)=> i.name)).join(", ")}`,
                     imageUrl: '',
                     link: {
                         mobileWebUrl: 'https://developers.kakao.com',
                         webUrl: 'https://developers.kakao.com',
                     },
+                }]
+            }
+            content=[...content, {
+                title: '출석률 변동 : ',
+                description: `${(data.lastDayGraph*100).toFixed(1)}% -> ${(data.todayGraph*100).toFixed(1)}% ( ${(data.todayGraph*100-data.lastDayGraph*100).toFixed(1)}% )`,
+                imageUrl: '',
+                link: {
+                    mobileWebUrl: 'https://developers.kakao.com',
+                    webUrl: 'https://developers.kakao.com',
                 },
-                {
-                    title: '취미의 특징, 탁구',
-                    description: '스포츠',
-                    imageUrl: '',
-                    link: {
-                        mobileWebUrl: 'https://developers.kakao.com',
-                        webUrl: 'https://developers.kakao.com',
-                    },
-                },
-            ]
+            },]
+            console.log(content);
             window.Kakao.Link.sendDefault({
                 objectType : 'list',
                 headerTitle: '출석체크 마감',
@@ -50,7 +60,7 @@ function kakao(){
         }
         else{
             const title=`${now.getHours()}시 ${now.getMinutes()}분 출석알림`;
-            const desc = (now.getMinutes() > 40) ? "미출석자 : 안녕 방가워 내이름은 하니" : "아래 버튼을 클릭하면 바로 출석이 완료됩니다. "
+            const desc = (now.getMinutes() > 40) ? `미출석자 : ${(data.studentResponses.map((i : any)=> i.name)).join(", ")}` : "아래 버튼을 클릭하면 바로 출석이 완료됩니다. "
             window.Kakao.Link.sendDefault({
                 objectType : 'feed',
                 content: { 
