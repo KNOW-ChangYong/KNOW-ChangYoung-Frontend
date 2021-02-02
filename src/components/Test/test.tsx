ee0bb364a8b074e9f36034f93147ed59383a5f43
eungyeole <eungyeole@naver.com>
2021-02-03 06:53:40 +0900

    UPDATE ;

--- src/components/Test/test.tsx
+++ src/components/Test/test.tsx
@@ -1,18 +1,18 @@
 import { start } from "repl";
 
-function test(){
+function test(){ 
     function getFormatDate(date: Date){
         var year = date.getFullYear();
-        var month = (1 + date.getMonth());
+        var month:string | number = (1 + date.getMonth());
         month = month >= 10 ? month : '0' + month;
-        var day = date.getDate();
+        var day: string |number = date.getDate();
         day = day >= 10 ? day : '0' + day;
-        return year + '-' + month + '-' + day;
+        return year + '/' + month + '/' + day;
     }
     function time(){
         const now=new Date();
         const startTime = new Date(`${getFormatDate(now)} 6:00:00`)
-        const endTime = new Date(`${getFormatDate(now)}} 8:01:00`)
+        const endTime = new Date(`${getFormatDate(now)} 8:01:00`)
         console.log(now.getDay)
         alert(now);
         alert(startTime);
